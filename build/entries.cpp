95b53e27d81e96e6e0552b2dca08b5d7b16825b5
yuchen <xyuc12en@gmail.com>
2025-01-06 13:33:29 -0500

    ignores

--- build/entries.cpp
+++
@@ -1,7 +0,0 @@
-void entries(){
-  TFile* f = new TFile("Statistics.root", "READ");
-  TTree* tree = (TTree*)f->Get("Detector");
-  int nEntry = tree->GetEntries();
-
-  std::cout<<nEntry<<std::endl;
-}
